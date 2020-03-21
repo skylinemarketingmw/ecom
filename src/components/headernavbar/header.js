3cd274cae81fdfcd0d89bd1dda7dfcf013056b80
skylinemarketingmw <skylinemarketingmw@gmail.com>
2020-03-21 17:58:18 -0500

    Styled header and navbar components

--- src/components/headernavbar/header.js
+++ src/components/headernavbar/header.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
@@ -6,7 +7,7 @@ class Header extends Component {
     render() {
         return (
             <div className='header'>
-                <img src='http://via.placeholder.com/50x50'/>
+                <img className='header__img' src='http://via.placeholder.com/50x50'/>
                 <div className='header__links'>
                 {
                     this.props.headerLinks.map((link, index) => {
